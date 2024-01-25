d3d2036743b7e1af2c183a168e6cc5220c02c0d3
asi0000 <asi0000@nate.com>
2024-01-25 10:51:42 +0900

    240125 review 1

---
+++ javaBasic/src/ch02/review/A_Constructor.java
@@ -0,0 +1,10 @@
+package ch02.review;
+
+public class A_Constructor {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ javaBasic/src/ch02/review/A_Instance.java
@@ -0,0 +1,10 @@
+package ch02.review;
+
+public class A_Instance {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
